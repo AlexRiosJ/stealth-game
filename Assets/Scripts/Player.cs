5608c52ea5c054c9ccc908460a69316f9d7edd50
Alejandro Rios <jassoalex97@hotmail.com>
2020-01-06 21:39:02 -0600

    Adding Player controller basics

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed = 7;
+    public float smoothMoveTime = 0.1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
+
+    void Update () {
+        Vector3 inputDirection = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized;
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp (smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+
+        float targetAngle = Mathf.Atan2 (inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle (angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+        transform.eulerAngles = Vector3.up * angle;
+
+        transform.Translate (transform.forward * smoothInputMagnitude * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
